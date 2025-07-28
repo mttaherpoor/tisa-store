1b3218a59348fa8b3902c5761989d16ebbbe111b
Taher <131670439+mttaherpoor@users.noreply.github.com>
2025-07-28 13:04:19 +0330

    implement product_list

--- products/models.py
+++ products/models.py
@@ -11,22 +11,31 @@ from django_ckeditor_5.fields import CKEditor5Field
 class Category(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, blank=True)
-    top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, blank=True, null=True, related_name='+')
+    top_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, blank=True, null=True, related_name="+"
+    )
 
     def __str__(self):
         return self.title
 
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="products"
+    )
     slug = models.SlugField()
-    description = CKEditor5Field(_('Product description'), config_name='extends')
+    description = CKEditor5Field(_("Product description"), config_name="extends")
     short_description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.PositiveIntegerField(blank=True)
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
-    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+    image = models.ImageField(
+        _("Product Image"),
+        upload_to="product/product_cover/",
+        blank=True,
+    )
     active = models.BooleanField(default=True)
-    
+
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
@@ -34,7 +43,7 @@ class Product(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('product_detail', args=[self.pk])
+        return reverse("product_detail", args=[self.pk])
 
 
 class ActiveCommentsManger(models.Manager):
@@ -44,22 +53,28 @@ class ActiveCommentsManger(models.Manager):
 
 class Comment(models.Model):
     PRODUCT_STARS = [
-        ('1', _('Very Bad')),
-        ('2', _('Bad')),
-        ('3', _('Normal')),
-        ('4', _('Good')),
-        ('5', _('Perfect')),
+        ("1", _("Very Bad")),
+        ("2", _("Bad")),
+        ("3", _("Normal")),
+        ("4", _("Good")),
+        ("5", _("Perfect")),
     ]
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Comment author'
+        related_name="comments",
+        verbose_name="Comment author",
     )
-    body = models.TextField(verbose_name=_('Comment Text'))
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
+    body = models.TextField(verbose_name=_("Comment Text"))
+    stars = models.CharField(
+        max_length=10, choices=PRODUCT_STARS, verbose_name=_("What is your score?")
+    )
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
@@ -71,4 +86,4 @@ class Comment(models.Model):
     active_comments_manager = ActiveCommentsManger()
 
     def get_absolute_url(self):
-        return reverse('product_detail', args=[self.product.id])
+        return reverse("product_detail", args=[self.product.id])
--- products/views.py
+++ products/views.py
@@ -5,27 +5,33 @@ from django.utils.translation import gettext as _
 from django.contrib import messages
 
 
-from .models import Product, Comment
+from .models import Product, Comment, Category
 from .forms import CommentForm
 
 
 class ProductListView(generic.ListView):
     # model = Product
     queryset = Product.objects.filter(active=True)
-    template_name = 'products/product_list.html'
-    context_object_name = 'products'
+    template_name = "products/product_list.html"
+    context_object_name = "products"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
 
     # def get_queryset(self):
     #
 
+
 class ProductDetailView(generic.DetailView):
     model = Product
-    template_name = 'products/product_detail.html'
-    context_object_name = 'product'
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comment_form'] = CommentForm()
+        context["comment_form"] = CommentForm()
         return context
 
 
@@ -40,11 +46,10 @@ class CommentCreateView(generic.CreateView):
         obj = form.save(commit=False)
         obj.author = self.request.user
 
-        product_id = int(self.kwargs['product_id'])
+        product_id = int(self.kwargs["product_id"])
         product = get_object_or_404(Product, id=product_id)
         obj.product = product
 
-        messages.success(self.request, _('Comment successfully created'))
+        messages.success(self.request, _("Comment successfully created"))
 
         return super().form_valid(form)
-
